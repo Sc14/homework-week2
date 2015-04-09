db5dfee7769ffec7d5c0fbdf47f420f93fb6445c
Sc14 <diogo6173@gmail.com>
2015-04-09 15:08:17 +0100

    Exercicio 1.1
    Class Email

---
+++ exercise1/email.rb
@@ -0,0 +1,45 @@
+# class Email
+# Exercicio 1.1
+#
+# by Diogo Silva - a6562
+#
+# Ref: http://justintallant.com/classes-in-ruby-using-getters-and-setters/?doing_wp_cron=1428585008.2999188899993896484375
+# Ref: 01-ruby-basics.pdf
+
+class Email
+
+  #Construtor
+  def initialize(subject,date,from)
+    @subject = subject
+    @date = date
+    @from = from
+  end
+
+  #Propriedades (getters & setters)
+  def subject=(subject)
+    @subject = subject
+  end
+  def subject
+    @subject
+  end
+
+  def date=(date)
+    @date = date
+  end
+  def date
+    @date
+  end
+
+  def from=(from)
+    @from = from
+  end
+  def from
+    @from
+  end
+end
+
+email = Email.new("Homework this week", "2014-12-01", "Ferdous")
+
+puts "Date:    #{email.date}"
+puts "From:    #{email.from}"
+puts "Subject: #{email.subject}"
