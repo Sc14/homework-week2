61bd9f8c8cfe024033f9b04a643727ecc5aaa78b
Sc14 <diogo6173@gmail.com>
2015-04-09 15:10:31 +0100

    Exericio 1.2
    Exericio 1.2
    Class Email com hash's

---
+++ exercise1/email2.rb
@@ -0,0 +1,42 @@
+# class Email
+# Exercicio 1.2
+#
+# by Diogo Silva - a6562
+
+class Email
+
+  #Construtor
+  def initialize(subject,headers)
+    @subject = subject
+    @date = headers[:date]
+    @from = headers[:from]
+  end
+
+  #Propriedades (getters & setters)
+  def subject=(subject)
+    @subject = subject
+  end
+  def subject
+    @subject
+  end
+
+  def date=(date)
+    @date = date
+  end
+  def date
+    @date
+  end
+
+  def from=(from)
+    @from = from
+  end
+  def from
+    @from
+  end
+end
+
+email = Email.new("Keep on coding! :)", { :date => "2014-12-01", :from => "Ferdous" })
+
+puts "Date:    #{email.date}"
+puts "From:    #{email.from}"
+puts "Subject: #{email.subject}"
